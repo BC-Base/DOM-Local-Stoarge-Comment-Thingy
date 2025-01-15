2b5e5a72cc46193cbc07843f345ac87b1bacd49f
BC-Base <brianc390@nycstudents.net>
2025-01-15 12:14:31 -0500

    Update script.js
    thingy

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  The variable "let clickCount" is an important part of the code. This creates the variable clickCount, which is what we will use to keep track of the amount of times the button has been pressed, which will them be displahyed later.
 const clickCountDisplay = document.getElementById('click-count');
         const header = document.getElementById('header');
         const button = document.getElementById('click-button');
@@ -8,6 +9,7 @@ const clickCountDisplay = document.getElementById('click-count');
         updateUI();
 
 //2.Explain what this event listener function does (and what function it calls).
+//This event listener function keeps track for if the button is being pressed or not. When the button is pressed, it calls the clickCount function, adding 1 to the amount in order to keep the current number it has up to date. This number will then be stored, ensuring that the number will always be kept and not lost.
         // Add event listener to button
         button.addEventListener('click', () => {
             clickCount++;
@@ -17,6 +19,8 @@ const clickCountDisplay = document.getElementById('click-count');
         });
 
 //3.Explain what function updateUI does and is holding the colors and texts
+//The function updateUI 
+
         function updateUI() {
             const colors = ['#FF5733', '#33FF57', '#3357FF', '#F5A623', '#E91E63'];
             const texts = [
